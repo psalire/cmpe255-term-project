37d78e58f200714ebb1297e573807fb1853f4414
psalire <philip.salire@sjsu.edu>
2021-03-29 23:55:04 -0700

    script to build new dataset

--- nba-api/get_cumulative_data.py
+++ nba-api/get_cumulative_data.py
@@ -5,12 +5,22 @@ from api import API as NBA_API
 # import pandas as pd
 sys.path.append('../')
 from datasets import data as Local ## Local module to get dataframes
-from pprint import PrettyPrinter
+# from pprint import PrettyPrinter
 
 DATASETS_DIR = '../datasets/'
 
 class DatasetBuilder:
-    """Keeps track of games visited per team to use with the nba api"""
+    """
+        Keeps track of games visited per team to use with the nba api
+        REQUIRED: cumulative_games_stats.csv must be empty except for the required headers:
+                  DATE,CITY,NICKNAME,TEAM_ID,W,L,W_HOME,
+                  L_HOME,W_ROAD,L_ROAD,TEAM_TURNOVERS,
+                  TEAM_REBOUNDS,GP,GS,ACTUAL_MINUTES,
+                  ACTUAL_SECONDS,FG,FGA,FG_PCT,FG3,FG3A,
+                  FG3_PCT,FT,FTA,FT_PCT,OFF_REB,DEF_REB,
+                  TOT_REB,AST,PF,STL,TOTAL_TURNOVERS,BLK,
+                  PTS,AVG_REB,AVG_PTS,DQ
+    """
 
     def __init__(self, dataset_dir=DATASETS_DIR):
         self.stats_df = Local.get_cumulative_games_stats_dataframe(DATASETS_DIR)
@@ -32,7 +42,7 @@ class DatasetBuilder:
             self.visited_game_ids[team_id][season] = set()
         self.visited_game_ids[team_id][season].add(game_id)
 
-    def get_team_stats(self, team_id, season):
+    def _get_team_stats(self, team_id, season):
         """Calls API to get cumulative stats using visited_game_ids"""
 
         team_id=str(team_id)
@@ -44,9 +54,24 @@ class DatasetBuilder:
             season
         )
 
-    def update_dataframe(self, date, json, save_to_csv='cumulative_games_stats.csv'):
+    def update_dataframe(self, date, team_id, season):
         """Updates dataframe with json"""
 
+        # Fetch json
+        print(f'[INFO] Fetching JSON for {date}, teamID-{team_id}, season-{season}:')
+        json = self._get_team_stats(team_id, season)
+        stats_vals = json['resultSets'][1]['rowSet'][0]
+        stats_dict = dict(zip(self.stats_df.columns[1:],stats_vals))
+
+        # Update dataframe
+        self.stats_df = self.stats_df.append(
+            {'DATE': date, **stats_dict},
+            ignore_index=True,
+        )
+
+    def save_dataframe_to_csv(self, filename):
+        """Save dataframe to csv at self.dataset_dir/filename"""
+        self.stats_df.to_csv(self.dataset_dir+filename)
 
 def main():
     """Main script"""
@@ -60,34 +85,33 @@ def main():
         'VISITOR_TEAM_ID',
         'SEASON',
     ]]
+    # Endpoint only supports 2017 season and after
+    games_df = games_df[games_df['SEASON'] >= 2017]
 
     dataset_builder = DatasetBuilder()
-    # dataset_builder.add_game_id('0022000677','1610612737','2020')
 
-    i=0
+    requests = 0
     for _, row in games_df.iterrows():
-        dataset_builder.add_game_id(
-            row['GAME_ID'],
-            row['HOME_TEAM_ID'],
-            row['SEASON'],
-        )
-        dataset_builder.add_game_id(
-            row['GAME_ID'],
-            row['VISITOR_TEAM_ID'],
-            row['SEASON'],
-        )
-        if i==50:
-            break
-        i+=1
-    PrettyPrinter().pprint(dataset_builder.visited_game_ids)
+        requests+=1
+        print(f'REQUEST #{requests}')
+        # Update dataframe for both home & visiting team
+        for team_id in [row['HOME_TEAM_ID'], row['VISITOR_TEAM_ID']]:
+            # Add entry
+            dataset_builder.add_game_id(
+                row['GAME_ID'],
+                team_id,
+                row['SEASON'],
+            )
+            # Update dataframe, and save csv every 10 requests
+            dataset_builder.update_dataframe(
+                row['GAME_DATE_EST'],
+                team_id,
+                row['SEASON'],
+            )
+            if requests%10 == 0:
+                dataset_builder.save_dataframe_to_csv('cumulative_games_stats.csv')
 
-    # dataset_builder = DatasetBuilder()
-    # dataset_builder.add_game_id('0022000677','1610612737','2020')
-    # dataset_builder.add_game_id('0022000660','1610612737','2020')
-    # dataset_builder.add_game_id('0022000640','1610612737','2020')
-    # print(dataset_builder.visited_game_ids)
-    #
-    # PrettyPrinter().pprint(dataset_builder.get_team_stats('1610612737','2020'))
+    dataset_builder.save_dataframe_to_csv('cumulative_games_stats.csv')
 
 if __name__=='__main__':
     main()
