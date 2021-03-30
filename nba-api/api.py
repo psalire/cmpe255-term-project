42de1485984e47adfbfbf4b0f1afd4479591c347
psalire <philip.salire@sjsu.edu>
2021-03-29 21:46:48 -0700

    mv nba api to separate file

---
+++ nba-api/api.py
@@ -0,0 +1,59 @@
+"""Use nba's cumulative api"""
+
+import time
+import requests
+import pandas as pd
+# from pprint import PrettyPrinter
+
+class API:
+    """Get jsons from nba api"""
+    # Headers needed to pass filter
+    __HEADERS = {
+        'Accept': 'application/json, text/plain, */*',
+        'x-nba-stats-origin': 'stats',
+        'x-nba-stats-token': 'true',
+        'Origin': 'https://www.nba.com',
+        'Referer': 'https://www.nba.com/',
+    }
+
+    def __init__(self):
+        self.req = requests.Session()
+        self.req.headers.update({
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0',
+        })
+
+    def __get_json(self, url, headers=None):
+        """Return a json from a get request"""
+
+        print('GET... ')
+        time.sleep(1.5) ## Sleep before request to not flood api
+        res = self.req.get(url, headers=headers if headers else {})
+        print(res.status_code)
+        # print(res.json())
+        return res.json()
+
+    def get_cumulative_team_stats(
+        self, game_ids, team_id, season, season_type='Regular Season', league_id='00'
+    ):
+        """Call endpoint to get cumulative team stats"""
+
+        game_ids = '|'.join([s.zfill(10) for s in game_ids])
+        return self.__get_json(
+            'https://stats.nba.com/stats/cumestatsteam?'+
+            f'GameIDs={game_ids}&LeagueID={league_id}&Season={season}&'+
+            f'SeasonType={season_type}&TeamID={team_id}',
+            headers=self.__HEADERS,
+        )
+
+    def get_cumulative_player_stats(
+        self, game_ids, player_id, season, season_type='Regular Season', league_id='00'
+    ):
+        """Call endpoint to get cumulative player stats"""
+
+        game_ids = '|'.join([s.zfill(10) for s in game_ids])
+        return self.__get_json(
+            'https://stats.nba.com/stats/cumestatsplayer?'+
+            f'GameIDs={game_ids}&LeagueID={league_id}&PlayerID={player_id}&'+
+            f'Season={season}&SeasonType={season_type}',
+            headers=self.__HEADERS,
+        )
--- nba-api/get_cumulative_data.py
+++ nba-api/get_cumulative_data.py
@@ -1,70 +1,22 @@
 """Use nba's api to build datasets"""
 
-import time
-import requests
 import pandas as pd
+import sys
+sys.path.append('../')
+from datasets import data as Local ## Local module to get dataframes
+from api import API
 # from pprint import PrettyPrinter
 
-class API:
-    """Get jsons from nba api"""
-    # Headers needed to pass filter
-    __HEADERS = {
-        'Accept': 'application/json, text/plain, */*',
-        'x-nba-stats-origin': 'stats',
-        'x-nba-stats-token': 'true',
-        'Origin': 'https://www.nba.com',
-        'Referer': 'https://www.nba.com/',
-    }
-
-    def __init__(self):
-        self.req = requests.Session()
-        self.req.headers.update({
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0',
-        })
-
-    def __get_json(self, url, headers=None):
-        """Return a json from a get request"""
-
-        print('GET... ')
-        time.sleep(1.5) ## Sleep before request to not flood api
-        res = self.req.get(url, headers=headers if headers else {})
-        print(res.status_code)
-        # print(res.json())
-        return res.json()
-
-    def get_cumulative_team_stats(
-        self, game_ids, team_id, season, season_type='Regular Season', league_id='00'
-    ):
-        """Call endpoint to get cumulative team stats"""
-
-        game_ids = '|'.join([s.zfill(10) for s in game_ids])
-        return self.__get_json(
-            'https://stats.nba.com/stats/cumestatsteam?'+
-            f'GameIDs={game_ids}&LeagueID={league_id}&Season={season}&'+
-            f'SeasonType={season_type}&TeamID={team_id}',
-            headers=self.__HEADERS,
-        )
-
-    def get_cumulative_player_stats(
-        self, game_ids, player_id, season, season_type='Regular Season', league_id='00'
-    ):
-        """Call endpoint to get cumulative player stats"""
-
-        game_ids = '|'.join([s.zfill(10) for s in game_ids])
-        return self.__get_json(
-            'https://stats.nba.com/stats/cumestatsplayer?'+
-            f'GameIDs={game_ids}&LeagueID={league_id}&PlayerID={player_id}&'+
-            f'Season={season}&SeasonType={season_type}',
-            headers=self.__HEADERS,
-        )
+DATASETS_DIR = '../datasets/'
 
 def main():
     """Main script"""
 
     # Open games dataset
-    games_df = pd.read_csv('../datasets/games.csv')
-    output_df = pd.read_csv('../datasets/cumulative_games_stats.csv')
+    games_df, _ = Local.get_games_and_winners_dataframe(DATASETS_DIR)
+    output_df = Local.get_cumulative_games_stats_dataframe(DATASETS_DIR)
     # print(games_df.columns)
+    print(output_df.columns)
     # pp = PrettyPrinter()
     nba_api = API()
     # nba_api.get_cumulative_team_stats(['0012000034'],'1610612759','2020')
