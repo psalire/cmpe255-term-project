f69a001de24bac20421b6fe06bc4a29a93294b6d
psalire <philip.salire@sjsu.edu>
2021-04-30 23:09:05 -0700

    fix dataset

--- datasets/build_combined_cumulative_dataset.py
+++ datasets/build_combined_cumulative_dataset.py
@@ -22,22 +22,52 @@ class CombinedDatasetBuilder(DatasetBuilder):
             [k+'_away' for k in self.avg_cols]
         )
 
-    def add_combined_current_values_to_df(self, home_team_id, away_team_id, season):
+    def add_combined_current_values_to_df(self, input_row):
         """Add row to dataframe with values of current dict val"""
 
+        home_team_id = input_row['HOME_TEAM_ID']
+        away_team_id = input_row['VISITOR_TEAM_ID']
+        season = input_row['SEASON']
+
         row = {}
-        exclude = {'sum_FG_PCT_total','sum_FT_PCT_total','sum_FG3_PCT_total','AT_HOME'}
+        exclude = {'sum_FG_PCT_total','sum_FT_PCT_total','sum_FG3_PCT_total','AT_HOME','GAMES_WON'}
         shared = {'DATE','GAME_ID','SEASON_TYPE','SEASON'}
-        for k_home in self.cumulative_stats[home_team_id][season]:
-            if k_home not in exclude.union(shared):
-                row[k_home+'_home'] = self.cumulative_stats[home_team_id][season][k_home]
-        for k_away in self.cumulative_stats[away_team_id][season]:
-            if k_away not in exclude.union(shared):
-                row[k_away+'_away'] = self.cumulative_stats[away_team_id][season][k_away]
-        for k_shared in shared:
-            assert self.cumulative_stats[away_team_id][season][k_shared] == \
-                    self.cumulative_stats[home_team_id][season][k_shared]
-            row[k_shared] = self.cumulative_stats[away_team_id][season][k_shared]
+
+        home_entry_exists = home_team_id in self.cumulative_stats and \
+                            season in self.cumulative_stats[home_team_id]
+        away_entry_exists = away_team_id in self.cumulative_stats and \
+                            season in self.cumulative_stats[away_team_id]
+
+        if home_entry_exists:
+            for k_home in self.cumulative_stats[home_team_id][season]:
+                if k_home not in exclude.union(shared):
+                    row[k_home+'_home'] = self.cumulative_stats[home_team_id][season][k_home]
+        else:
+            for k_home in self.stats_df_columns:
+                if k_home not in exclude.union(shared):
+                    row[k_home+'_home'] = 0
+                row['TEAM_ID_home'] = home_team_id
+                row['W_PCT_home'] = 0
+                row['GAMES_PLAYED_home'] = 0
+
+        if away_entry_exists:
+            for k_away in self.cumulative_stats[away_team_id][season]:
+                if k_away not in exclude.union(shared):
+                    row[k_away+'_away'] = self.cumulative_stats[away_team_id][season][k_away]
+        else:
+            for k_away in self.stats_df_columns:
+                if k_away not in exclude.union(shared):
+                    row[k_away+'_away'] = 0
+                row['TEAM_ID_away'] = away_team_id
+                row['W_PCT_away'] = 0
+                row['GAMES_PLAYED_away'] = 0
+
+        # Shared vals
+        row['GAME_ID'] = input_row['GAME_ID']
+        row['SEASON_TYPE'] = str(row['GAME_ID'])[0]
+        row['DATE'] = input_row['GAME_DATE_EST']
+        row['SEASON'] = input_row['SEASON']
+
         # row['HOME_TEAM_ID'] = home_team_id
         # row['VISITOR_TEAM_ID'] = away_team_id
 
@@ -54,16 +84,13 @@ def main():
     rows=0
     print('Start...')
     for _, row in combined_dataset_builder.games_df.iterrows():
+        combined_dataset_builder.add_combined_current_values_to_df(row)
         combined_dataset_builder.accumulate_values(row)
-        combined_dataset_builder.add_combined_current_values_to_df(
-            row['HOME_TEAM_ID'],
-            row['VISITOR_TEAM_ID'],
-            row['SEASON']
-        )
         rows+=1
+        # if rows%30 == 0:
+        #     break
         if rows%1000 == 0:
             print(f'Row {rows}...')
-            # dataset_builder.save_to_csv()
     combined_dataset_builder.save_to_csv()
 
 if __name__=='__main__':
--- datasets/build_cumulative_dataset.py
+++ datasets/build_cumulative_dataset.py
@@ -1,6 +1,7 @@
 """Build cumulative dataset from games.csv"""
 
 import pandas as pd
+# import numpy as np
 import data as Local ## Local module to get dataframes
 
 DATASETS_DIR = '../datasets/'
@@ -9,7 +10,8 @@ class DatasetBuilder:
     """Class to build cumulative dataframe from games.csv"""
 
     def __init__(self, datasets_dir=DATASETS_DIR, out_filename='out'):
-        self.games_df, _ = Local.get_games_and_winners_dataframe(DATASETS_DIR)
+        games_data = Local.get_games_and_targets_dataframe(DATASETS_DIR)
+        self.games_df = games_data[0]
         self.ranking_df = Local.get_ranking_dataframe(DATASETS_DIR)
         self.cumulative_stats = {}
         # Don't add averages, only countable values
@@ -21,11 +23,11 @@ class DatasetBuilder:
             'GAMES_PLAYED','PTS_total','AST_total','REB_total',
         }
 
-        self.stats_df = pd.DataFrame(columns= \
+        self.stats_df_columns = \
             ['DATE','GAME_ID','TEAM_ID','SEASON_TYPE','SEASON','AT_HOME']+ \
             list(self.countable_cols) + \
             list(self.avg_cols)
-        )
+        self.stats_df = pd.DataFrame(columns=self.stats_df_columns)
         self.datasets_dir = datasets_dir
         self.out_filename = out_filename
 
@@ -33,24 +35,28 @@ class DatasetBuilder:
         """Add values to existing dict val, or add to dict if DNE"""
 
         for team_id, home_team in zip(
-            [row['HOME_TEAM_ID'],row['VISITOR_TEAM_ID']], [True,False]
+            [row['HOME_TEAM_ID'],row['VISITOR_TEAM_ID']],
+            [True,False]
         ):
+
             if team_id not in self.cumulative_stats:
+                # Add entry for team if DNE
                 self.cumulative_stats[team_id] = {}
             if row['SEASON'] not in self.cumulative_stats[team_id]:
+                # Add entry for sesason if DNE
                 self.cumulative_stats[team_id][row['SEASON']] = {
                     'TEAM_ID': str(team_id),
                     'SEASON': row['SEASON'],
                     'GAMES_PLAYED': 0,
+                    'GAMES_WON': 0
                 }
-            self.cumulative_stats[team_id][row['SEASON']]['GAME_ID']=str(row['GAME_ID'])
-            self.cumulative_stats[team_id][row['SEASON']]['SEASON_TYPE']=str(row['GAME_ID'])[0]
-            self.cumulative_stats[team_id][row['SEASON']]['DATE']=row['GAME_DATE_EST']
-            self.cumulative_stats[team_id][row['SEASON']]['AT_HOME']=home_team
-            self.cumulative_stats[team_id][row['SEASON']]['W_PCT']= self.ranking_df[
-                (self.ranking_df['TEAM_ID']==team_id) &
-                (self.ranking_df['STANDINGSDATE']==row['GAME_DATE_EST'])
-            ]['W_PCT'].iloc[0]
+            team_stats_dict = self.cumulative_stats[team_id][row['SEASON']]
+
+            # Add stats values
+            team_stats_dict['GAME_ID']=str(row['GAME_ID'])
+            team_stats_dict['SEASON_TYPE']=str(row['GAME_ID'])[0]
+            team_stats_dict['DATE']=row['GAME_DATE_EST']
+            team_stats_dict['AT_HOME']=home_team
 
             tag = 'home' if home_team else 'away'
             none_val = False
@@ -62,17 +68,27 @@ class DatasetBuilder:
                 'sum_FT_PCT_{}',
                 'sum_FG3_PCT_{}',
             ]:
+                # Add computed stats values
+
                 curr_key = key.format('total')
                 val = row[key.format(tag).replace('sum_','')]
                 if str(val)=='nan':
                     val = 0
                     none_val = True
-                if curr_key in self.cumulative_stats[team_id][row['SEASON']]:
-                    self.cumulative_stats[team_id][row['SEASON']][curr_key] += val
+                if curr_key in team_stats_dict:
+                    team_stats_dict[curr_key] += val
                 else:
-                    self.cumulative_stats[team_id][row['SEASON']][curr_key] = val
+                    team_stats_dict[curr_key] = val
 
-            self.cumulative_stats[team_id][row['SEASON']]['GAMES_PLAYED']+=int(not none_val)
+            team_stats_dict['GAMES_PLAYED'] += int(not none_val)
+            team_stats_dict['GAMES_WON'] += int(
+                (row['PTS_home']>row['PTS_away']) if home_team \
+                else (row['PTS_away']>row['PTS_home'])
+            )
+            if team_stats_dict['GAMES_PLAYED']:
+                team_stats_dict['W_PCT'] = team_stats_dict['GAMES_WON']/team_stats_dict['GAMES_PLAYED']
+            else:
+                team_stats_dict['W_PCT'] = 0.0
 
             # Calculate averages
             key_and_totals = [
@@ -83,25 +99,41 @@ class DatasetBuilder:
                 ('APG_total','AST_total'),
                 ('RPG_total','REB_total'),
             ]
-            if self.cumulative_stats[team_id][row['SEASON']]['GAMES_PLAYED']>0:
+            if team_stats_dict['GAMES_PLAYED']>0:
                 for key,tot in key_and_totals:
-                    self.cumulative_stats[team_id][row['SEASON']][key] = \
-                        self.cumulative_stats[team_id][row['SEASON']][tot] / \
-                        self.cumulative_stats[team_id][row['SEASON']]['GAMES_PLAYED']
+                    team_stats_dict[key] = \
+                        team_stats_dict[tot] / \
+                        team_stats_dict['GAMES_PLAYED']
             else:
                 for key,tot in key_and_totals:
-                    self.cumulative_stats[team_id][row['SEASON']][key] = 0
+                    team_stats_dict[key] = 0
 
 
-    def add_current_values_to_df(self, team_id, season):
+    def add_current_values_to_df(self, team_id, row_vals):
         """Add row to dataframe with values of current dict val"""
-
-        self.stats_df = self.stats_df.append(
-            {
+        season = row_vals['SEASON']
+        if team_id in self.cumulative_stats and season in self.cumulative_stats[team_id]:
+            current_vals = {
                 k:self.cumulative_stats[team_id][season][k] \
                 for k in self.cumulative_stats[team_id][season] \
-                if k not in {'sum_FG_PCT_total','sum_FT_PCT_total','sum_FG3_PCT_total'}
-            },
+                if k not in {'GAMES_WON','sum_FG_PCT_total','sum_FT_PCT_total','sum_FG3_PCT_total'}
+            }
+        else:
+            # If values dne, i.e. first game of season
+            current_vals = {k:0 for k in self.stats_df_columns}
+            current_vals['TEAM_ID'] = team_id
+            current_vals['W_PCT'] = 0
+            current_vals['GAMES_PLAYED'] = 0
+
+        # Use metadata of current game, not previous
+        current_vals['AT_HOME'] = team_id==row_vals['HOME_TEAM_ID']
+        current_vals['GAME_ID'] = row_vals['GAME_ID']
+        current_vals['SEASON_TYPE'] = str(row_vals['GAME_ID'])[0]
+        current_vals['DATE'] = row_vals['GAME_DATE_EST']
+        current_vals['SEASON'] = season
+
+        self.stats_df = self.stats_df.append(
+            current_vals,
             ignore_index=True,
         )
 
@@ -118,13 +150,15 @@ def main():
     rows=0
     print('Start...')
     for _, row in dataset_builder.games_df.iterrows():
-        dataset_builder.accumulate_values(row)
         for team_id in [row['HOME_TEAM_ID'],row['VISITOR_TEAM_ID']]:
-            dataset_builder.add_current_values_to_df(team_id, row['SEASON'])
+            dataset_builder.add_current_values_to_df(team_id, row)
+        dataset_builder.accumulate_values(row)
         rows+=1
+        # if rows%60 == 0:
+        #     break
         if rows%1000 == 0:
             print(f'Row {rows}...')
-            # dataset_builder.save_to_csv()
+            dataset_builder.save_to_csv()
     dataset_builder.save_to_csv()
 
 if __name__=='__main__':
